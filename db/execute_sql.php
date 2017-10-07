d64e4743165214d1f5881604f9e17974c729dc2a
petercz1 <email.peter.carroll@gmail.com>
2017-10-07 18:41:37 +0800

    You attempt things that you do not even plan because of your extreme stupidity.

--- db/execute_sql.php
+++ db/execute_sql.php
@@ -3,16 +3,16 @@
 include 'errors.php';
 include 'credentials.php';
 
-// try {
-//     $conn = new PDO("mysql:host='localhost'", $username, $password);
-//     // set the PDO error mode to exception
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     $sql = "CREATE DATABASE myDBPDO";
-//     // use exec() because no results are returned
-//     $conn->exec($sql);
-//     echo "Database created successfully<br>";
-// } catch (PDOException $e) {
-//     echo $sql . "<br>" . $e->getMessage();
-// }
+try {
+    $conn = new PDO("mysql:host='localhost'", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE DATABASE myDBPDO";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Database created successfully<br>";
+} catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
 
 $conn = null;
