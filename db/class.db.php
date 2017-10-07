d35c68e3f7232d16d8dc5a56e703838aa9f6fc25
petercz1 <email.peter.carroll@gmail.com>
2017-10-07 20:14:13 +0800

    "Plan to throw one away.  You will anyway." 		-- Fred Brooks, "The Mythical Man Month"

--- db/class.db.php
+++ db/class.db.php
@@ -7,6 +7,19 @@ include 'credentials.php';
 logger('making a db connection');
 
 class Db{
-
-    
+public function db_conn(){
+        try {
+            $conn = new PDO("mysql:host=localhost", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "CREATE DATABASE scrap2";
+            // use exec() because no results are returned
+            $conn->exec($sql);
+            logger('Database created successfully<br>');
+            echo "Database created successfully<br>";
+        } catch (PDOException $e) {
+            logger($e->getMessage());
+            echo json_encode($e->getMessage());
+        }
+    }
 }
