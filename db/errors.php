21121a627764804f996b0cc28f3fe3e408793ee6
petercz1 <email.peter.carroll@gmail.com>
2017-10-07 19:48:31 +0800

    Linux!  Guerrilla UNIX Development     Venimus, Vidimus, Dolavimus. 		-- Mark A. Horton KA4YBR, mah@ka4ybr.com

--- db/errors.php
+++ db/errors.php
@@ -36,7 +36,7 @@ function logger($message)
 {
     global $config;
     $debug_arr = debug_backtrace();
-    file_put_contents($config["app_dir"] . "notices.log", 'line ' . $debug_arr[0]['line'] . ' ('.$debug_arr[0]['file'] .') ' . $message . PHP_EOL, FILE_APPEND);
+    file_put_contents($config["app_dir"] . "notices.log", 'line ' . $debug_arr[0]['line'] . ' ('. basename($debug_arr[0]['file']) .') ' . $message . PHP_EOL, FILE_APPEND);
 }
 
 register_shutdown_function("check_for_fatal");
