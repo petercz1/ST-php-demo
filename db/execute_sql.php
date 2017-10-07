f3b80180b2061b0cf59e90e8f257d36d0b07be98
petercz1 <email.peter.carroll@gmail.com>
2017-10-07 20:05:38 +0800

    Monogamy is the Western custom of one wife and hardly any mistresses. 		-- H. H. Munro

--- db/execute_sql.php
+++ db/execute_sql.php
@@ -8,11 +8,13 @@ try {
     $conn = new PDO("mysql:host=localhost", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE DATABASE scrap";
+    $sql = "CREATE DATABASE scrap2";
     // use exec() because no results are returned
     $conn->exec($sql);
+    logger('Database created successfully<br>');
     echo "Database created successfully<br>";
 } catch (PDOException $e) {
+    logger($e->getMessage());
     echo json_encode($e->getMessage());
 }
 
