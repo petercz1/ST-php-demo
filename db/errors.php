fd1ffc940a9f458d46625ec36f1d28ec494b8c32
petercz1 <email.peter.carroll@gmail.com>
2017-10-07 18:46:59 +0800

    Yes, we have consensus that we need 64 bit support.  :-) 		-- Larry Wall in <199710291922.LAA07101@wall.org>

--- db/errors.php
+++ db/errors.php
@@ -13,6 +13,7 @@ function log_error($num, $str, $file, $line, $context = null)
 */
 function log_exception(Exception $e)
 {
+    global $config;
     $message = "Line {$e->getLine()} in " . basename($e->getFile()) . ", {$e->getMessage()}  (" . get_class($e) . ")";
     file_put_contents($config["app_dir"] . "./errors.log", '');
     file_put_contents($config["app_dir"] . "./errors.log", $message . PHP_EOL, FILE_APPEND);
