e5830606021a0669dbcd3c2344a19f2baca1f43f
petercz1 <email.peter.carroll@gmail.com>
2017-10-07 20:01:21 +0800

    It is easier to make a saint out of a libertine than out of a prig. 		-- George Santayana

--- db/execute_sql.php
+++ db/execute_sql.php
@@ -8,7 +8,7 @@ try {
     $conn = new PDO("mysql:host='localhost'", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE DATABASE myDBPDO";
+    $sql = "CREATE DATABASE scrap";
     // use exec() because no results are returned
     $conn->exec($sql);
     echo "Database created successfully<br>";
