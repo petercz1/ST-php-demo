92278ce947261fe3dd9cf0ecd62185472cf957bd
petercz1 <email.peter.carroll@gmail.com>
2017-10-07 16:45:04 +0800

    Your conscience never stops you from doing anything.  It just stops you from enjoying it.

--- js/javascript.js
+++ js/javascript.js
@@ -4,5 +4,10 @@ $(document).ready(do_setup);
 
 function do_setup(){
     console.log('Inside do_setup');
-    $().click();
+    $('#submit').click(run_command);
+}
+
+function run_command(){
+    console.log('Inside run_command');
+    
 }
