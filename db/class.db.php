29fd0f93c53341b37afed4b6e3ef1e5ed05b31df
petercz1 <email.peter.carroll@gmail.com>
2017-10-07 22:01:47 +0800

    When this load is DONE I think I'll wash it AGAIN ...

--- db/class.db.php
+++ db/class.db.php
@@ -7,14 +7,14 @@ logger('making a db connection');
 
 class Db
 {
-    global $username;
-    global $password;
     public function conn()
     {
+        global $username;
+        global $password;
         try {
             $conn = new \PDO("mysql:host=localhost", $username, $password);
             // set the PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conn->setAttribute(\PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $sql = "CREATE DATABASE scrap4";
             // use exec() because no results are returned
             $conn->exec($sql);
