fddd96bf3e21f56ee0c7db0d14753df4fb29641b
petercz1 <email.peter.carroll@gmail.com>
2017-10-08 20:05:19 +0800

    A few hours grace before the madness begins again.

--- js/javascript.js
+++ js/javascript.js
@@ -37,6 +37,7 @@ function dropped_db(data) {
 
 function success(echo_results) {
   console.log('Inside success');
+  do_setup();
   $('#submit').removeClass().addClass('btn btn-success right').text('success!');
   console.log(echo_results);
 }
