6faf26fd053b2623da04a0cba1ab29d1ab9c6180
petercz1 <email.peter.carroll@gmail.com>
2017-10-08 18:13:19 +0800

    Don't look back, the lemmings are gaining on you.

---
+++ db/show_databases.php
@@ -0,0 +1,21 @@
+<?php
+namespace chipbug\php_demo;
+
+include 'errors.php';
+include 'credentials.php';
+include 'class.mysqli.php';
+
+logger('testing');
+logger($_GET['db']);
+$name = $_GET['db'];
+
+$db = new DbConnect($username, $password);
+$sql = "CREATE DATABASE $name";
+if ($db->conn->query($sql)) {
+    logger('created db');
+    echo 'created db';
+} else {
+    logger($db->conn->error);
+    echo $db->conn->error;
+}
+$db->kill();
