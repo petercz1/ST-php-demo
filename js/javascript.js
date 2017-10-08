76316ee9fff7d556a26c9e351c4e228f0e92fd02
petercz1 <email.peter.carroll@gmail.com>
2017-10-08 19:40:34 +0800

    "It's a summons." 	"What's a summons?" 	"It means summon's in trouble." 		-- Rocky and Bullwinkle

--- js/javascript.js
+++ js/javascript.js
@@ -65,7 +65,7 @@ function show_dbs(data){
         console.log(db);
         td.text(db.Database);
         tr.append(td);
-        td.html('<button class="btn btn-danger">delete</button>')
+        td.html('<button class="btn btn-danger">delete</button>');
         $('.table tbody').append(tr);
     });
 
