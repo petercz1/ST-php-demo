34c4104c1144f0f760429b11256e79e6296c183d
petercz1 <email.peter.carroll@gmail.com>
2017-10-08 16:07:22 +0800

    Dinner suggestion #302 (Hacker's De-lite): 	1 tin imported Brisling sardines in tomato sauce 	1 pouch Chocolate Malt Carnation Instant Breakfast 	1 carton milk

--- db/class.db.php
+++ db/class.db.php
@@ -11,12 +11,13 @@ class Db
     public function create_db($name)
     {
         try {
+            logger('passing ' . $name);
             global $username, $password, $servername;
             // Create connection
             $conn = new \mysqli($servername, $username, $password);
 
             // Create database
-            $sql = "CREATE DATABASE test";
+            $sql = "CREATE DATABASE $$name";
             $result = $conn->query($sql);
             logger($result);
             // close conn
