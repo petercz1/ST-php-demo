3c344494bce4787d26f045c4ae8d58cc35707574
petercz1 <email.peter.carroll@gmail.com>
2017-10-08 16:58:51 +0800

    The human race has one really effective weapon, and that is laughter. 		-- Mark Twain

--- db/class.mysqli.php
+++ db/class.mysqli.php
@@ -5,19 +5,26 @@ include 'errors.php';
 include 'credentials.php';
 include 'class.dbconnect.php';
 
-// Create connection
-$conn = new mysqli('localhost', $username, $password);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+class DbConnect
+{
+    public $conn;
+    public function __construct($username, $password)
+    {
+        // Create connection
+        $conn = new mysqli('localhost', $username, $password);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
 
-// Create database
-$sql = "CREATE DATABASE myDB";
-if ($conn->query($sql) === true) {
-    echo "Database created successfully";
-} else {
-    echo "Error creating database: " . $conn->error;
-}
+        // Create database
+        $sql = "CREATE DATABASE myDB";
+        if ($conn->query($sql) === true) {
+            echo "Database created successfully";
+        } else {
+            echo "Error creating database: " . $conn->error;
+        }
 
-$conn->close();
+        $conn->close();
+    }
+}
--- db/execute_sql.php
+++ db/execute_sql.php
@@ -3,7 +3,7 @@ namespace chipbug\php_demo;
 
 include 'errors.php';
 include 'credentials.php';
-include 'class.dbconnect.php';
+include 'class.mysqli.php';
 
 logger('testing');
 logger($_GET['sql']);
