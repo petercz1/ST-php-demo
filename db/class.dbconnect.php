353e332dfd7df549003b5017db574d2c802f81a9
petercz1 <email.peter.carroll@gmail.com>
2017-10-08 16:12:43 +0800

    The optimist thinks that this is the best of all possible worlds, and the pessimist knows it. 		-- J. Robert Oppenheimer, "Bulletin of Atomic Scientists"

--- db/class.dbconnect.php
+++ db/class.dbconnect.php
@@ -6,4 +6,16 @@ use PDO;
 
 class Db
 {
+    public function connect()
+    {
+        try {
+            $conn = new PDO("mysql:host=$servername", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            logger("Database connection created successfully");
+        } catch (PDOException $ex) {
+            logger($ex->getMessage());
+        }
+        $conn = null;
+    }
 }
