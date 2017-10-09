9d1a85a167789ed343b9c42c03a96f39c8ec2695
petercz1 <email.peter.carroll@gmail.com>
2017-10-09 11:11:15 +0800

    It is a hard matter, my fellow citizens, to argue with the belly, since it has no ears. 		-- Marcus Porcius Cato

--- backend/common/errors.php
+++ backend/common/errors.php
@@ -28,8 +28,8 @@ function logger($message)
     global $config;
     $debug_arr = debug_backtrace();
     $prepend = 'line ' . $debug_arr[0]['line'] . ' ('. basename($debug_arr[0]['file']) .') ' . print_r($message, true) . PHP_EOL;
-    $fileContents = file_get_contents($config["app_dir"] . "backend/common/notices.log");
-    file_put_contents($config["app_dir"] . "notices.log", $prepend.$fileContents);
+    $fileContents = file_get_contents("backend/common/notices.log");
+    file_put_contents($config["app_dir"] . "backend/common/notices.log", $prepend.$fileContents);
 }
 
 register_shutdown_function("check_for_fatal");
