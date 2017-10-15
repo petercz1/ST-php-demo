f7c0ac16df4be41c23aa239908da78c43eabecb0
petercz1 <email.peter.carroll@gmail.com>
2017-10-15 08:31:56 +0800

    <chesty> xemacs fixed my flatulence 		-- From the "XEmacs: Not just an editor" department

--- backend/routes/router.php
+++ backend/routes/router.php
@@ -1,20 +1,3 @@
 <?php
 
 include '../common/logger.php';
-
-$uri = 'dd';
-$router = array();
-
-$router['home'] = function () {
-    echo '<h1>home page</h1>';
-};
-
-$router['databases'] = function () {
-    echo 'doing database stuff...';
-};
-
-if (isset($router[$uri])) {
-    $router[$uri]();
-} else {
-    echo '<h1>404 not found. sorry about that...</h1>';
-}
