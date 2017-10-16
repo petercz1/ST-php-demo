f62896dc68e8fb98e4d6e1e8d11a231a670d675b
petercz1 <email.peter.carroll@gmail.com>
2017-10-17 06:45:01 +0800

    Amoebit: 	Amoeba/rabbit cross; it can multiply and divide at the same time.

--- js/users.js
+++ js/users.js
@@ -10,6 +10,33 @@ function do_setup() {
   $('#user_name').click(reset);
 }
 
+function get_users() {
+    console.log('Inside get_users');
+    $.get('backend/users/show_users.php').done(show_users).fail(blow_up);
+}
+
+function show_users(users) {
+    console.log('Inside show_users');
+    $('.table tbody').html('');
+    console.log(users);
+    var users = JSON.parse(users); // convert text response to JSON
+    $.each(users, function(index, user) {
+        var tr = $('<tr>');
+        var td = $('<td>'); // make first <td>, add db name
+        td.text(user.User);
+        tr.append(td);
+        td = $('<td>'); //make second <td>, add button with id of dbase name
+        var btn = $('<button class="delete_user" id=' + user.User + '>');
+        btn.text('delete');
+        btn.addClass("btn btn-danger");
+        td.append(btn);
+        tr.append(td);
+        $('.table tbody').append(tr);
+    });
+    $('.delete_user').click(drop_user);
+
+}
+
 function create_user() {
   console.log('Inside create_user');
   $('#submit').removeClass().addClass('btn btn-warning right').text('submitting...');
@@ -19,7 +46,14 @@ function create_user() {
     user: userName,
     pass: userPass
   };
-  $.get('backend/users/create_user.php', data).done(success).fail(oops);
+  $.get('backend/users/create_user.php', data).done(created_user).fail(blow_up);
+}
+
+function created_user(echo_results) {
+    console.log('created user');
+    $('#submit').removeClass().addClass('btn btn-success right').text('success!');
+    console.log(echo_results);
+    do_setup();
 }
 
 function drop_user() {
@@ -29,23 +63,16 @@ function drop_user() {
   var data = {
     user: userName
   };
-  $.get('backend/users/drop_user.php', data).done(dropped_user).fail(oops);
+  $.get('backend/users/drop_user.php', data).done(dropped_user).fail(blow_up);
 }
 
 function dropped_user(data) {
-  console.log('Inside dropped_user');
+  console.log('dropped user');
   console.log(data);
   do_setup();
 }
 
-function success(echo_results) {
-  console.log('Inside success');
-  $('#submit').removeClass().addClass('btn btn-success right').text('success!');
-  console.log(echo_results);
-  do_setup();
-}
-
-function oops(echo_results) {
+function blow_up(echo_results) {
   console.log('Inside oops');
   $('#submit').removeClass().addClass('btn btn-danger right').text('oops...');
   console.log(echo_results);
@@ -54,31 +81,4 @@ function oops(echo_results) {
 function reset() {
   console.log('Inside reset');
   $('#submit').removeClass().addClass('btn btn-primary right').text('submit');
-}
-
-function get_users() {
-  console.log('Inside get_users');
-  $.get('backend/users/show_users.php').done(show_users).fail(oops);
-}
-
-function show_users(users) {
-  console.log('Inside show_users');
-  $('.table tbody').html('');
-  console.log(users);
-  var users = JSON.parse(users); // convert text response to JSON
-  $.each(users, function(index, user) {
-    var tr = $('<tr>');
-    var td = $('<td>'); // make first <td>, add db name
-    td.text(user.User);
-    tr.append(td);
-    td = $('<td>'); //make second <td>, add button with id of dbase name
-    var btn = $('<button class="delete_user" id=' + user.User + '>');
-    btn.text('delete');
-    btn.addClass("btn btn-danger");
-    td.append(btn);
-    tr.append(td);
-    $('.table tbody').append(tr);
-  });
-  $('.delete_user').click(drop_user);
-
 }
